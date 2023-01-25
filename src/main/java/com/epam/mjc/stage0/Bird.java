4cc33e907d2e715dfd7863a26cafbeb379380f01
Aziz1019 <starinc1998@gmail.com>
2023-01-25 13:58:27 +0500

    Initial Commit

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,22 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal {
+    private String colorBird;
+    private int numberOfPawsBird;
+    private boolean hasFurBird;
 
+    public Bird(String color, int numberOfPaws, boolean hasFur) {
+        super(color, numberOfPaws, hasFur);
+        this.colorBird = "blue";
+        this.numberOfPawsBird = 2;
+        this.hasFurBird = false;
+    }
+
+    public Bird() {
+    }
+
+    @Override
+    public String getDescription() {
+        return "This animal is mostly blue. It has 2 paws and no fur. Moreover, it has 2 wings and can fly.";
+    }
 }
--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -1,5 +1,26 @@
 package com.epam.mjc.stage0;
 
-public class Dog {
+public class Dog extends Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Dog() {
+    }
+
+    public Dog(String color, int numberOfPaws, boolean hasFur) {
+        super(color, numberOfPaws, hasFur);
+        this.color = "brown";
+        this.numberOfPaws = 4;
+        this.hasFur = true;
+    }
+
+
+
+
+
+    @Override
+    public String getDescription() {
+        return "This animal is mostly brown. It has 4 paws and a fur.";
+    }
 }
